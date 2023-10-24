4f9d94946fc22a82b81a8352d9ecedecbe7d0f30
Vaibhav Bisht <92950515+beastvaibhv@users.noreply.github.com>
2023-10-24 17:06:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 //your JS code here. If required.
+const arr = document.getElementByClassName("btn");
+for(let i = 0; i < arr.length; i++){
+	arr[i].addEventListener("click",expectPlayingAudio();)
+}
+const stop =  document.getElementByClassName("stop");
